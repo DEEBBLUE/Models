0fae9198303e06a6dadd6f9c001774256da9a6a6
Lifter <Sosite.my_dick@gmail.com>
2025-07-18 18:20:04 -0400

    Create GlobalConfig

---
+++ Config/Config.go
@@ -0,0 +1,32 @@
+package serviceConfig
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+type GlobalConfig struct {
+	DBService  ServiceConfig `yaml:"dbService"`
+	ExService  ServiceConfig `yaml:"exService"`
+	SsoService ServiceConfig `yaml:"ssoService"`
+	ApiGetAway ServiceConfig `yaml:"ApiGetAway"`
+}
+
+type ServiceConfig struct {
+	Host string `yaml: "host"`
+	Port int    `yaml: "port"`
+}
+
+func InitConfig(path string) GlobalConfig {
+	var conf GlobalConfig
+	data,err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}	
+
+	if err := yaml.Unmarshal(data,&conf);err != nil {
+		panic(err)
+	}
+	return conf
+}
