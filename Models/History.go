9ea41ec7106fe2dbac07b665af4a4de833164042
Lifter <Sosite.my_dick@gmail.com>
2025-07-31 01:00:23 -0400

    Add optimize,take out alloc mem for slice

--- Models/History.go
+++ Models/History.go
@@ -10,14 +10,19 @@ type UserHistory struct{
 }
 
 func(hist *UserHistory) CreateGRPC() []*Types.Exchange {
-	var resHistory []*Types.Exchange
+	hLen := len(hist.History)
+	resHistory := make([]*Types.Exchange,0,hLen)
+
 	for _,ex := range hist.History{
 		resHistory = append(resHistory, ex.CreateGRPC())
 	}		
 
 	return resHistory
 }
+
 func(hist *UserHistory) CreateFromGRPC(history []*Types.Exchange){
+	hist.History = make([]Exchange,0,len(history))
+
 	for _,ex := range history{
 		var exchange Exchange
 		exchange.CreateFromGRPC(ex)
